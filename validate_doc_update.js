c36188ffd47a41281d839a16a1b7ba7ca7fd6402
llabball <thewhorn@googlemail.com>
2013-09-11 22:58:49 +0200

    added basic security
    Currently only admins will be able to delete docs or change important properties.

---
+++ validate_doc_update.js
@@ -0,0 +1,19 @@
+function(newDoc, oldDoc, userCtx, secObj) {
+  var v = require("commonjs/validate").init(newDoc, oldDoc, userCtx, secObj)
+    , is_admin = v.isAdmin()
+ 
+  if (newDoc._deleted === true) {
+    if (is_admin) {
+      return
+    } else {
+      v.unauthorized('Only admins may delete other user annotations.')
+    }
+  }
+
+  if (is_admin) return
+
+  v.require('consumer','id','quote','ranges','uri','user')
+  v.unchanged('user')
+  v.unchanged('uri')
+  
+}
